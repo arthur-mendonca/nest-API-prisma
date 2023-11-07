7d8263c4dd735cb8bb0caca212665a6854a1986a
arthur-mendonca <arthu91@gmail.com>
2023-11-07 19:38:15 -0300

    feat: CRUD - conexão com db - Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+/* eslint-disable prettier/prettier */
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule, UsersModule],
+    controllers: [AppController],
+    providers: [AppService],
+
 })
+
 export class AppModule {}
+
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import {  Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onApplicationShutdown() {
+    await this.$disconnect()
+  }
+  
+}
+
